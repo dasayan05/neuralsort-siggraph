61f0a0bb0fa77d5b17b462081be1712c8deac351
Das, Ayan (PG/R - Elec Electronic Eng) <a.das@surrey.ac.uk>
2020-01-02 18:07:02 +0000

    metric viz plot enhanced; a bug fixed

--- metricviz.py
+++ metricviz.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import splrep, splev, interp1d
 
+INTERP = 'cubic'
+
 def main( args ):
     fig = plt.figure()
     with open(args.metricfile, 'rb') as f:
@@ -10,17 +12,22 @@ def main( args ):
         c = np.linspace(0., 1., num = 100)
         r_ac, o_ac, p_ac = np.zeros_like(c), np.zeros_like(c), np.zeros_like(c)
         for i, (r, o, p) in enumerate(zip(R, O, P)):
-            r_spl = interp1d(r[:,0], r[:,1])
-            o_spl = interp1d(o[:,0], o[:,1])
-            p_spl = interp1d(p[:,0], p[:,1])
+            r_spl = interp1d(r[:,0], r[:,1], kind=INTERP)
+            o_spl = interp1d(o[:,0], o[:,1], kind=INTERP)
+            p_spl = interp1d(p[:,0], p[:,1], kind=INTERP)
             
             r_ac = (i * r_ac + r_spl(c)) / (i + 1)
             o_ac = (i * o_ac + o_spl(c)) / (i + 1)
             p_ac = (i * p_ac + p_spl(c)) / (i + 1)
         
-        plt.plot(r_ac, c, color='r')
-        plt.plot(o_ac, c, color='g')
-        plt.plot(p_ac, c, color='b')
+        plt.plot(c, r_ac, color='r')
+        plt.plot(c, o_ac, color='g')
+        plt.plot(c, p_ac, color='b')
+        plt.legend(['random order', 'human order', 'model order'])
+        plt.xlabel('sketch completion percentage')
+        plt.ylabel('class recognition accuracy')
+        if args.category != '':
+            plt.title(f'class: {args.category}')
         plt.savefig(args.outfile)
     plt.close()
 
@@ -30,6 +37,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--metricfile', type=str, required=True, help='The metric file')
     parser.add_argument('-o', '--outfile', type=str, required=True, help='Output graph file')
+    parser.add_argument('-c', '--category', type=str, required=False, default='', help='name of category')
     args = parser.parse_args()
 
     main( args )
